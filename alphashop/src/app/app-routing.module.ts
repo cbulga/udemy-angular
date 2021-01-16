31f6d426e6b3879d0684b5e8389195d1dfa5b212
Cristian Bulgarelli <cristian.bulgarelli@tasgroup.eu>
2021-01-16 11:56:45 +0100

    JWT authentication with roles (lezione 128)

--- alphashop/src/app/app-routing.module.ts
+++ alphashop/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Ruoli } from 'src/models/Ruoli';
 import { ArticoliComponent } from './articoli/articoli.component';
 import { ErrorComponent } from './error/error.component';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import { NewartComponent } from './newart/newart.component';
@@ -12,11 +14,12 @@ const routes: Routes = [
   {'path': '', 'component': LoginComponent },
   {'path': 'index', 'component': LoginComponent },
   {'path': 'login', 'component': LoginComponent },
-  {'path': 'welcome/:userid', 'component': WelcomeComponent, canActivate: [RouteGuardService] },
-  {'path': 'articoli', 'component': ArticoliComponent, canActivate: [RouteGuardService] },
-  {'path': 'articoli/:filter', 'component': ArticoliComponent, canActivate: [RouteGuardService] },
-  {'path': 'newart/:codArt', 'component': NewartComponent, canActivate: [RouteGuardService] },
+  {'path': 'welcome/:userid', 'component': WelcomeComponent, canActivate: [RouteGuardService], data: { roles: [Ruoli.utente]} },
+  {'path': 'articoli', 'component': ArticoliComponent, canActivate: [RouteGuardService], data: { roles: [Ruoli.utente]} },
+  {'path': 'articoli/:filter', 'component': ArticoliComponent, canActivate: [RouteGuardService], data: { roles: [Ruoli.utente]} },
+  {'path': 'newart/:codArt', 'component': NewartComponent, canActivate: [RouteGuardService], data: { roles: [Ruoli.amministratore]} },
   {'path': 'logout', 'component': LogoutComponent, canActivate: [RouteGuardService] },
+  {path:'forbidden', component : ForbiddenComponent},
   {'path': '**', 'component': ErrorComponent },
 ];
 
--- alphashop/src/app/services/route-guard.service.ts
+++ alphashop/src/app/services/route-guard.service.ts
@@ -1,20 +1,38 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import { AuthappService } from './authapp.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { AuthJWTService } from './auth-jwt.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RouteGuardService implements CanActivate {
 
-  constructor(private authService: AuthappService, private router: Router) { }
+  token : string = '';
+  ruoli : string[];
+
+  constructor(private authService: AuthJWTService, private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    this.token = this.authService.getAuthToken();
+
+    const helper = new JwtHelperService();
+    const decodedToken = helper.decodeToken(this.token);
+    this.ruoli = decodedToken['authorities'];
+
     if (!this.authService.isLoggedIn()) {
       this.router.navigate(["login"])
       return false;
+    } else {
+      if (route.data.roles == null || route.data.roles.length === 0)
+        return true;
+      else if (this.ruoli.some(r => route.data.roles.includes(r)))
+        return true;
+      else {
+        this.router.navigate(['forbidden']);
+        return false;
+      }
     }
-    return true;
   }
 }
