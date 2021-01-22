e201c8eed6bfe3f2cb36ad4a78b60fea3c7d7c1b
Cristian Bulgarelli <cristian.bulgarelli@tasgroup.eu>
2021-01-22 16:04:54 +0100

    JUnit con authenticazione JWT (lezione 146)

--- articoli-web-service/src/test/java/com/xantrix/webapp/unittest/controllertest/SelectArtTest.java
+++ articoli-web-service/src/test/java/com/xantrix/webapp/unittest/controllertest/SelectArtTest.java
@@ -16,31 +16,66 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.xantrix.webapp.Application;
+
+import net.minidev.json.JSONObject;
 
 
 @ContextConfiguration(classes = Application.class)
 @SpringBootTest
 @TestMethodOrder(OrderAnnotation.class)
-class SelectArtTest
-{
+class SelectArtTest {
 	private MockMvc mockMvc;
-		
+
+	private String authServerUrl = "http://localhost:9100/auth";
+	private String tokenJWT;
+	private String username = "Nicola";
+	private String password = "123Stella";
+	
 	@Autowired
 	private WebApplicationContext wac;
+
+	private final ObjectMapper objectMapper = new ObjectMapper();
 	
 	@BeforeEach
 	public void setup() throws JSONException, IOException {
 		mockMvc = MockMvcBuilders
 				.webAppContextSetup(wac)
-				.build();	
+				.build();
+
+		getTokenFromAuthServer();
+	}
+
+	private void getTokenFromAuthServer() throws JsonMappingException, JsonProcessingException {
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		JSONObject userJson = new JSONObject();
+		userJson.put("username", username);
+		userJson.put("password", password);
+
+		HttpEntity<String> request = new HttpEntity<>(userJson.toString(), headers);
+
+		String jsonToken = restTemplate.postForObject(authServerUrl, request, String.class);
+		JsonNode root = objectMapper.readTree(jsonToken);
+		String token = root.path("token").asText();
+
+		tokenJWT = "Bearer " + token;
 	}
 
 	String jsonData =  
@@ -53,6 +88,7 @@ class SelectArtTest
 			"    \"pesoNetto\": 1.5,\n" + 
 			"    \"idStatoArt\": \"1\",\n" + 
 			"    \"dataCreaz\": \"2010-06-14\",\n" + 
+			"    \"prezzo\": 1.07,\n" + // <-- Aggiunto il prezzo dell'articolo
 			"    \"barcode\": [\n" + 
 			"        {\n" + 
 			"            \"barcode\": \"8008490000021\",\n" + 
@@ -75,6 +111,7 @@ class SelectArtTest
 	@Order(1)
 	void listArtByEan() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/api/articoli/cerca/ean/8008490000021")
+				.header("Authorization", tokenJWT)
 				.accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
@@ -117,13 +154,14 @@ class SelectArtTest
 				
 				.andDo(print());
 	}
-	
+
 	private String barcode = "8008490002138";
-	
+
 	@Test
 	@Order(2)
 	void errlistArtByEan() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/api/articoli/cerca/ean/" + barcode)
+				.header("Authorization", tokenJWT)
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(jsonData)
 				.accept(MediaType.APPLICATION_JSON))
@@ -132,24 +170,26 @@ class SelectArtTest
 				.andExpect(jsonPath("$.messaggio").value("Non è stato trovato alcun articolo avente il barcode " + barcode))
 				.andDo(print());
 	}
-	
+
 	@Test
 	@Order(3)
 	void listArtByCodArt() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/api/articoli/cerca/codice/002000301")
+				.header("Authorization", tokenJWT)
 				.accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
 				.andExpect(content().json(jsonData)) 
 				.andReturn();
 	}
-	
+
 	private String codArt = "002000301b";
 	
 	@Test
 	@Order(4)
 	void errlistArtByCodArt() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/api/articoli/cerca/codice/" + codArt)
+				.header("Authorization", tokenJWT)
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(jsonData)
 				.accept(MediaType.APPLICATION_JSON))
@@ -158,13 +198,14 @@ class SelectArtTest
 				.andExpect(jsonPath("$.messaggio").value("L'articolo con codice " + codArt + " non è stato trovato!"))
 				.andDo(print());
 	}
-	
+
 	private String jsonData2 = "[" + jsonData + "]";
 
 	@Test
 	@Order(5)
 	void listArtByDesc() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/api/articoli/cerca/descrizione/ACQUA ULIVETO 15 LT")
+				.header("Authorization", tokenJWT)
 				.accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(jsonPath("$", hasSize(1)))
@@ -177,6 +218,7 @@ class SelectArtTest
 	@Order(6)
 	void listArtByDesc2() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/api/articoli/cerca/descrizione/ULIVETO")
+				.header("Authorization", tokenJWT)
 				.accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(jsonPath("$", hasSize(2)))
